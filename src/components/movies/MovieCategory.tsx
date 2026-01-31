c4a17d9d1ccd2a34ddf91380f878f6cd71b25a00
Quoc An <an.quoctran@crossian.com>
2026-01-31 21:26:19 +0700

    Refactor MovieCategory component for improved readability and consistency

--- src/components/movies/MovieCategory.tsx
+++ src/components/movies/MovieCategory.tsx
@@ -14,50 +14,56 @@ type MovieCategoryProps = {
 };
 const MovieCategory: FC<MovieCategoryProps> = (props) => {
   const { movies, pathAll, slidesPerView = 4, title } = props;
-  const swiperRef = useRef<SwiperCore>();
+  const swiperRef = useRef<SwiperCore>(null);
   const srcImage = movies?.seoOnPage?.og_image || [];
 
   return (
-    <div className='max-w-7xl mx-auto px-5'>
-      <div className='flex items-center justify-between mb-6 mt-12'>
-        <h3 className='text-2xl md:text-3xl font-extrabold'>{title}</h3>
-        <div className='flex items-center rounded-full border-2 border-white/10 text-white'>
-          <button className='px-3 py-1.5' onClick={() => swiperRef.current?.slidePrev()}>
+    <div className="max-w-7xl mx-auto px-5">
+      <div className="flex items-center justify-between mb-6 mt-12">
+        <h3 className="text-2xl md:text-3xl font-extrabold">{title}</h3>
+        <div className="flex items-center rounded-full border-2 border-white/10 text-white">
+          <button
+            className="px-3 py-1.5"
+            onClick={() => swiperRef.current?.slidePrev()}
+          >
             <svg
-              className='w-6 h-6 text-white'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='none'
-              viewBox='0 0 24 24'
+              className="w-6 h-6 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
             >
               <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='m15 19-7-7 7-7'
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m15 19-7-7 7-7"
               />
             </svg>
           </button>
-          <span className='w-0.5 h-6 rounded bg-white/10' />
-          <button className='px-3 py-1.5' onClick={() => swiperRef.current?.slideNext()}>
+          <span className="w-0.5 h-6 rounded bg-white/10" />
+          <button
+            className="px-3 py-1.5"
+            onClick={() => swiperRef.current?.slideNext()}
+          >
             <svg
-              className='w-6 h-6 text-white'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='none'
-              viewBox='0 0 24 24'
+              className="w-6 h-6 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
             >
               <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='m9 5 7 7-7 7'
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m9 5 7 7-7 7"
               />
             </svg>
           </button>
@@ -96,19 +102,25 @@ const MovieCategory: FC<MovieCategoryProps> = (props) => {
       {pathAll && (
         <Link
           href={pathAll}
-          className='flex items-center px-5 py-2 border-2 border-primary w-max mx-auto mt-8 hover:bg-primary duration-150 hover:text-black font-bold'
+          className="flex items-center px-5 py-2 border-2 border-primary w-max mx-auto mt-8 hover:bg-primary duration-150 hover:text-black font-bold"
         >
           Xem Tất Cả
           <svg
-            className='w-6 h-6 text-white'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            fill='none'
-            viewBox='0 0 24 24'
+            className="w-6 h-6 text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
           >
-            <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m9 5 7 7-7 7' />
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m9 5 7 7-7 7"
+            />
           </svg>
         </Link>
       )}
